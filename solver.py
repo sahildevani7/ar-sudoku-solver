0360cd288260d3699ecdd66dd0ded03ddbb7a541
Sahil <sahil.devani05@gmail.com>
2023-07-12 21:35:43 +0530

    backtracking algo

---
+++ solver.py
@@ -0,0 +1,58 @@
+# Finds all empty spaces in a 9x9 2D matrix board where 0 indicates an empty space
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+# Validates if the board can be solved
+def valid(board, num, pos):
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    box_x = pos[1] // 3
+    box_y = pos[0] // 2
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+    
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+    
+    for i in range(1,10):
+        if valid(board, i, (row,col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+# Takes an unsolved sudoku board and returns a fully solved board
+def get_board(bo):
+    if solve(bo):
+        return bo
+    else:
+        raise ValueError
+
+
+
+
+
+
+
+
